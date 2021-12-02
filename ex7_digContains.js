f2bdefde74f6538773ed70d8e353066e0724cc4e
Gayane25 <gayabennu@gmail.com>
2021-12-02 22:03:53 +0400

    solved with function and includes method

--- ex7_digContains.js
+++ ex7_digContains.js
@@ -1,1 +1,12 @@
 //. Insert a digit and a number. Check whether the digits contains in the number or not.
+
+function check(num1, num2) {
+  num1 = num1.toString();
+  num2 = num2.toString();
+  if (num2.includes(num1)) {
+    console.log('yes');
+  } else {
+    console.log('no');
+  }
+}
+check(8, 45689);
