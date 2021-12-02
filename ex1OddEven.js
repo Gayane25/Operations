7a8796c4fc9bf6142b161dca549e62119bb6db12
Gayane25 <gayabennu@gmail.com>
2021-12-02 21:16:58 +0400

    written by using for loop

--- ex1OddEven.js
+++ ex1OddEven.js
@@ -1,1 +1,10 @@
 //Given a number. Print “odd” if the number is odd and “even” if it is even.
+
+let array = [125, 35, 20];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 === 0) {
+    console.log(array[i], 'is', 'even');
+  } else {
+    console.log(array[i], 'is', 'odd');
+  }
+}
