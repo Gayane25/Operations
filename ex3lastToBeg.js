e27f6a05c413d9765aa0409bd15872fee829f212
Gayane25 <gayabennu@gmail.com>
2021-12-02 21:26:44 +0400

    solving with if statement and math operation

--- ex3lastToBeg.js
+++ ex3lastToBeg.js
@@ -1,2 +1,12 @@
 /*. Given a positive integer. Bring the last digit of the number to the beginning. Print the new number. If
 the last digit of the inserted number is 0, number remains the same. */
+
+let posInt = 367;
+if (posInt % 10 === 0) {
+  console.log(posInt);
+} else {
+  let newInt = (posInt % 10).toString();
+  posInt = Math.floor(posInt / 10).toString();
+  newInt += posInt;
+  console.log(newInt);
+}
