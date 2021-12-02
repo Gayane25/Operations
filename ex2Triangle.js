8df56ea102ef943761aebf094f00d71bbd18f647
Gayane25 <gayabennu@gmail.com>
2021-12-02 21:21:26 +0400

    completed task by using function

--- ex2Triangle.js
+++ ex2Triangle.js
@@ -1,2 +1,9 @@
 /*Given two variables, which are the angles of a triangle. Find the third angle of the triangle. (Sum of
 the angles of a triangle equals 180 degrees).*/
+
+function triangle(angle1, angle2) {
+  let sumOfAngles = 180;
+  let third = sumOfAngles - (angle1 + angle2);
+  return third;
+}
+console.log(triangle(30, 30));
