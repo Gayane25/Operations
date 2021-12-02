2e42ccb016f0f923597bdcf0640e5e88e06b02b3
Gayane25 <gayabennu@gmail.com>
2021-12-03 00:52:29 +0400

    solved first task from Optional

---
+++ optionalTask.js
@@ -0,0 +1,10 @@
+//. 1. Enter a number. Find the difference between its biggest and smallest digits.
+
+let number = 2453;
+
+let numStr = number.toString().split('');
+let maxVal = Math.max(...numStr);
+let minVal = Math.min(...numStr);
+console.log(maxVal - minVal);
+
+//2.Convert base-2 number to base-10
