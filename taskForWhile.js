0e4f9c7e100aedec588ba9db9ce2de0de95802c1
Gayane25 <gayabennu@gmail.com>
2021-12-02 21:08:06 +0400

    added while loop

---
+++ taskForWhile.js
@@ -0,0 +1,14 @@
+/*. for vs for-in
+Write and do all the tasks from Literature Section
+try to write a the following loop with while and do-while:
+for (var i = 0; i < a.length; i++) {
+console.log("for", i, a[i]);
+} */
+
+// while loop
+let i = 0;
+let a = [1, 4, 10, 15, 20];
+while (i < a.length) {
+  i++;
+  console.log('for', i, a[i]);
+}
