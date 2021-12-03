ee3fd50046676e6b6cebdf05ad1ed4e29f6272a4
Gayane25 <gayabennu@gmail.com>
2021-12-03 21:02:43 +0400

    task solved by using loops

---
+++ ex12_MissingWords.js
@@ -0,0 +1,17 @@
+/*Given a sentence with missing words and an array of words. Replace all ‘_ʼ in a sentence with the
+words from the array.*/
+
+let newStr = '';
+let str1 = 'If at _ you donʼt _, try, try _.';
+let j = 0;
+let arr = ['first', 'succeed', 'again'];
+str1 = str1.split('');
+for (let i = 0; i < str1.length; i++) {
+  if (str1[i] === '_') {
+    newStr += arr[j];
+    j++;
+  } else {
+    newStr += str1[i];
+  }
+}
+console.log(newStr);
